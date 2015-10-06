9e0b6f1d864fe8d9d49521383bac4d7b9315ab07
tfwio <tfwroble@gmail.com>
2015-10-06 03:55:14 -0500

    Show settings in PropertyGrid when loaded form theme file

---
+++ SD-Addin/AnotherThemeTool/source/Src/Commands/LoadXamlTheme.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Markup;
+using ICSharpCode.Core;
+using ThemeTool;
+namespace ThemeTool.Commands
+{
+  public class LoadXamlTheme : AbstractMenuCommand
+  {
+    public override void Run()
+    {
+      String xamlFile = null;
+      
+      using (var writer = new System.IO.StringWriter()) {
+        using (var ofd = new System.Windows.Forms.OpenFileDialog() {
+                 Filter = "XAML File|*.xaml"
+               }) {
+          if (ofd.ShowDialog() != DialogResult.OK)
+            return;
+          xamlFile = ofd.FileName;
+        }
+      }
+      var xamlStr = System.IO.File.ReadAllText(xamlFile);
+      var pc = new ParserContext();
+      //
+      pc.XamlTypeMapper = new XamlTypeMapper(new string[] {  });
+      //
+      pc.XamlTypeMapper.AddMappingProcessingInstruction("ad", "AvalonDock", "AvalonDock");
+      pc.XamlTypeMapper.AddMappingProcessingInstruction("adRes", "AvalonDock.Properties", "AvalonDock");
+      pc.XamlTypeMapper.AddMappingProcessingInstruction("sys", "System", "mscorlib");
+      pc.XamlTypeMapper.AddMappingProcessingInstruction("th", "ThemeTool","AnotherThemeTool");
+      //
+      pc.XmlnsDictionary.Add("ad", "ad");
+      pc.XmlnsDictionary.Add("adRes", "adRes");
+      pc.XmlnsDictionary.Add("sys", "sys");
+      pc.XmlnsDictionary.Add("th", "th");
+      Assembly.LoadWithPartialName("AnotherThemeTool");
+//      pc.BaseUri = new Uri(@"clr-namespace:AnotherThemeTool",UriKind.RelativeOrAbsolute);
+      var rd = (ResourceDictionary)XamlReader.Parse(xamlStr);
+      //
+      AvalonDock.ThemeFactory.ResetTheme();
+//      System.Windows.Application.Current.Resources.MergedDictionaries.Add(
+//        new ResourceDictionary{Source=new Uri(@"/AvalonDock;component/themes/generic.xaml",UriKind.RelativeOrAbsolute)}
+//       );
+      System.Windows.Application.Current.Resources.MergedDictionaries.Add(rd);
+    }
+  }
+}
+
+
+
+
---
+++ SD-Addin/AnotherThemeTool/source/Src/StringConverter.cs
@@ -0,0 +1,82 @@
+﻿// Generated from: "$(projectdir)../artifacts/msdev2013theme.yaml"
+using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Security.Permissions;
+using System.Windows.Media;
+namespace ThemeTool
+{
+  public class GradientStopExpandableConverter : ExpandableObjectConverter
+  {
+    public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+    {
+      if (destinationType == typeof(GradientStop)) return true;
+      if (destinationType == typeof(GradientStopCollection)) return true;
+      if (destinationType == typeof(string)) return true;
+      return base.CanConvertTo(context, destinationType);
+    }
+    public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+    {
+      if (destinationType == typeof(GradientStopCollection) && value is string)
+        return string.Format("{0}",(value as GradientStopCollection).ToGradientStopString());
+      
+      if (destinationType == typeof(string) && value is GradientStop)
+        return string.Format(
+          "{0}:{1}",
+          (value as GradientStop).Color.ToHexString(true),
+          (value as GradientStop).Offset
+         );
+      
+      if (destinationType == typeof(GradientStop) && value is string)
+        return (value as string).ToGradientStop();
+      
+      if (destinationType == typeof(string) && value is GradientStopCollection)
+        return (value as GradientStopCollection).ToGradientStopString();
+      
+      return base.ConvertTo(context, culture, value, destinationType);
+    }
+    public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+    {
+      if (sourceType == typeof(string)) return true;
+      return base.CanConvertFrom(context, sourceType);
+    }
+    public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+    {
+      if (value is string)
+      {
+//        System.Windows.Forms.MessageBox.Show(string.Format("{0}\n{1}",value,context));
+        return (value as string).ToStops();
+      }
+      
+      return base.ConvertFrom(context, culture, value);
+    }
+    public override PropertyDescriptorCollection GetProperties(ITypeDescriptorContext context, object value, Attribute[] attributes)
+    {
+      return null;
+      return base.GetProperties(context, value, attributes);
+    }
+  }
+  [HostProtection(SecurityAction.LinkDemand, SharedState = true)]
+  public class GradientStopCollectionConverter : TypeConverter
+  {
+    public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+    {
+      return sourceType == typeof(string) || sourceType==typeof(GradientStopCollection) || base.CanConvertFrom(context, sourceType);
+    }
+
+    public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+    {
+      if (value is string) {
+        return (value as string).ToStops();
+      }
+      if (value is GradientStopCollection) {
+        return (value as GradientStopCollection).ToGradientStopString();
+      }
+      if (value == null) {
+        return "";
+      }
+      return base.ConvertFrom(context, culture, value);
+    }
+  }
+}
+
